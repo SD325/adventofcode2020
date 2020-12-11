e77dbb66788d3c6322124583ebb08bd9f9c8a2b9
SD325 <spandand515@gmail.com>
2020-12-11 14:43:49 -0500

    finished day 11

--- day11.py
+++ day11.py
@@ -1,20 +1,5 @@
-def isOcc(a, r, c):
-    return 0 <= r < len(a) and 0 <= c < len(a[0]) and a[r][c] == '#'
-
-
-def p1(a):
-    seats = [list(xx) for xx in a]
-    changed = False
-    adj = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-    for i in range(len(a)):
-        for j in range(len(a[0])):
-            if a[i][j] == 'L' and all(not isOcc(a, i+dr, j+dc) for dr, dc in adj):
-                seats[i][j] = '#'
-                changed = True
-            elif a[i][j] == '#' and sum(isOcc(a, i+dr, j+dc) for dr, dc in adj) >= 4:
-                seats[i][j] = 'L'
-                changed = True
-    return p1(seats) if changed else sum(s.count('#') for s in seats)
+def p1_check(a, i, j, dr, dc):
+    return 0 <= i+dr < len(a) and 0 <= j+dc < len(a[0]) and a[i+dr][j+dc] == '#'
 
 
 def p2_check(a, i, j, dr, dc):
@@ -29,23 +14,22 @@ def p2_check(a, i, j, dr, dc):
     return False
 
 
-
-def p2(a):
+def process(a, check, MIN_REQ):
     seats = [list(xx) for xx in a]
     changed = False
-    adj = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+    ADJ = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for i in range(len(a)):
         for j in range(len(a[0])):
-            if a[i][j] == 'L' and all(not p2_check(a, i, j, dr, dc) for dr, dc in adj):
+            if a[i][j] == 'L' and all(not check(a, i, j, dr, dc) for dr, dc in ADJ):
                 seats[i][j] = '#'
                 changed = True
-            elif a[i][j] == '#' and sum(p2_check(a, i, j, dr, dc) for dr, dc in adj) >= 5:
+            elif a[i][j] == '#' and sum(check(a, i, j, dr, dc) for dr, dc in ADJ) >= MIN_REQ:
                 seats[i][j] = 'L'
                 changed = True
-    return p2(seats) if changed else sum(s.count('#') for s in seats)
+    return process(seats, check, MIN_REQ) if changed else sum(s.count('#') for s in seats)
 
 
 with open("inputs/day11.txt") as f:
     inp = [list(l.strip()) for l in f.readlines()]
-    print(p1(inp))
-    print(p2(inp))
+    print(process(inp, p1_check, 4))
+    print(process(inp, p2_check, 5))
