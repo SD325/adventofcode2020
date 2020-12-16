f5ab059a7952905628cb92a97f2437c26ecc65e6
SD325 <spandand515@gmail.com>
2020-12-15 19:09:45 -0500

    finished day 14

--- day14.py
+++ day14.py
@@ -1,19 +1,33 @@
-def bitmask(v):
+def p1():
     res = list(mask[::-1])
-    rev_string = str(bin(v))[2:][::-1]
+    rev_string = str(bin(value))[2:][::-1]
     for i, x in enumerate(rev_string):
         res[i] = x if mask[len(mask) - i - 1] == 'X' else mask[len(mask) - i - 1]
     res = ['0' if x == 'X' else x for x in res]
-    return int(''.join(res[::-1]), 2)
+    mem[address] = int(''.join(res[::-1]), 2)
+
+
+# Source: https://github.com/gfvirga/python_challenges/blob/master/Advent%20of%20Code%202020/day14.py
+def p2():
+    addr = f'{address:036b}'
+    address_helper = [m if m == 'X' or m == a else '1' for a, m in zip(addr, mask)]
+    orig = address_helper[:]
+    for num in range(2 ** address_helper.count('X')):
+        for char in bin(num)[2:].zfill(address_helper.count('X')):
+            address_helper[address_helper.index('X')] = char
+        mem2[''.join(address_helper)] = value
+        address_helper = orig[:]
 
 
 with open("inputs/day14.txt") as f:
-    mem, mask = {}, ''
+    mem, mem2, mask = {}, {}, ''
     for line in f.readlines():
         if line[:3] == 'mem':
             address = int(line[line.index('[')+1:line.index(']')])
             value = int(line[line.index('=')+1:].strip())
-            mem[address] = bitmask(value)
+            p1()
+            p2()
         else:
             mask = line[line.index('=')+1:].strip()
     print(sum(mem.values()))
+    print(sum(mem2.values()))
